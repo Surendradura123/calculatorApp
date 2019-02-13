aada01613d32b4ce193aa8a2b136a5b68a9632f3
surendra dura <suren.dura@yahoo.com>
2019-02-13 12:48:57 +0000

    bn
    nm

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/calculate', :controller=>'calculator', :action=>'calculate'
+
+  post '/sum', :controller=>'calculator', :action=>'sum'
+
+  post '/subtract', :controller=>'calculator', :action=>'subtract'
+
+end
